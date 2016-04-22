9ad0a1b1527fa1800922320954bd75b4cb8cd5dc
Alex McArther <acmcarther@gmail.com>
2016-04-21 23:46:39 -0700

    Refactor blocking socket, add non-blocking socket (#3)
    * refactor blocking socket, add non-blocking socket
    
    * Fix the cuke

--- src/socket/mod.rs
+++ src/socket/mod.rs
@@ -1,21 +1,65 @@
-use std::io;
+use packet::{
+  CompleteGafferPacket,
+  GafferPacket
+};
 
-use addr::ToSingleSocketAddr;
+use connection::Connection;
 
-use packet::GafferPacket;
+use std::net::SocketAddr;
+
+use std::collections::HashMap;
 
 
 pub mod blocking;
 pub mod non_blocking;
 
-/// Highly redundant, ordered, non-congesting pseudo-udp protocol
-///
-/// TODO: More documentation
-///
-/// NOTE: This protocol is very prone to starvations -- it expects high throughput!
-pub trait GafferSocket: Sized {
-  fn bind<A: ToSingleSocketAddr>(addr: A) -> io::Result<Self>;
-  fn recv(&mut self) -> io::Result<GafferPacket>;
-  fn send(&mut self, packet: GafferPacket) -> io::Result<usize>;
+pub struct GafferState {
+  connections: HashMap<SocketAddr, Connection>
 }
 
+impl GafferState {
+  pub fn new() -> GafferState {
+    GafferState { connections: HashMap::new() }
+  }
+
+  pub fn preprocess_packet(&mut self, p: GafferPacket) -> (SocketAddr, Vec<u8>) {
+    let connection = self.connections.entry(p.addr).or_insert(Connection::new());
+    connection.waiting_packets.enqueue(connection.seq_num, p.clone());
+    let final_packet = helpers::assemble_packet(connection.seq_num, p.clone(), connection);
+    connection.seq_num = connection.seq_num.wrapping_add(1);
+    (p.addr, final_packet.serialized())
+  }
+
+  pub fn dropped_packets(&mut self, addr: SocketAddr) -> Vec<GafferPacket> {
+    let connection = self.connections.entry(addr).or_insert(Connection::new());
+    connection.dropped_packets.drain(..).collect()
+  }
+
+  fn receive(&mut self, addr: SocketAddr, packet: CompleteGafferPacket) -> GafferPacket {
+    let connection = self.connections.entry(addr).or_insert(Connection::new());
+    connection.their_acks.ack(packet.seq);
+    let dropped_packets = connection.waiting_packets.ack(packet.ack_seq, packet.ack_field);
+    connection.dropped_packets = dropped_packets.into_iter().map(|(_, p)| p).collect();
+    GafferPacket { addr: addr, payload: packet.payload }
+  }
+}
+
+
+
+pub mod helpers {
+  use connection::Connection;
+
+  use packet::{
+    CompleteGafferPacket,
+    GafferPacket
+  };
+
+  pub fn assemble_packet( seq_num: u16, p: GafferPacket, connection: &Connection) -> CompleteGafferPacket {
+    CompleteGafferPacket {
+      seq: seq_num,
+      ack_seq: connection.their_acks.last_seq,
+      ack_field: connection.their_acks.field,
+      payload: p.payload
+    }
+  }
+}
--- src/socket/non_blocking.rs
+++ src/socket/non_blocking.rs
@@ -1,13 +1,111 @@
-use std::collections::HashMap;
-
-use std::net::SocketAddr;
+use std::io;
 
 use mio::udp::UdpSocket;
 
-use connection::Connection;
+use socket::GafferState;
+use addr::ToSingleSocketAddr;
+
+use packet::{
+  CompleteGafferPacket,
+  GafferPacket
+};
 
 #[allow(dead_code)]
-pub struct NonBlockingGafferSocket {
+pub struct GafferSocket {
   udp_socket: UdpSocket,
-  connections: HashMap<SocketAddr, Connection>
+  state: GafferState
+}
+
+impl GafferSocket {
+  pub fn bind<A: ToSingleSocketAddr>(addr: A) -> io::Result<Self> {
+    let first_addr = addr.to_single_socket_addr().unwrap();
+    UdpSocket::bound(&first_addr).map(|sock| {
+      GafferSocket {
+        udp_socket: sock,
+        state: GafferState::new()
+      }
+    })
+  }
+
+  /// Receive a normal message
+  ///
+  /// - Get next message
+  /// - Add its sequence # to our memory
+  /// - Identify dropped packets from message header
+  /// - Forget own acked packets
+  /// - Enqueue Sure-Dropped packets into resubmit-queue
+  pub fn recv(&mut self) -> io::Result<Option<GafferPacket>> {
+    let mut res = [0; 1024];
+    self.udp_socket.recv_from(&mut res)
+      // TODO: Fix to_vec, it is suboptimal here
+      .and_then(|opt| {
+        match opt {
+          Some((_, addr)) => {
+            CompleteGafferPacket::deserialize(res.to_vec())
+              .map(|packet| Some(self.state.receive(addr, packet)))
+          },
+          None => Ok(None)
+        }
+      })
+  }
+
+  /// Send a normal message
+  ///
+  /// - Send dropped packets
+  /// - Send packet
+  pub fn send(&mut self, p: GafferPacket) -> io::Result<Option<usize>> {
+    let dropped_packets = self.state.dropped_packets(p.addr);
+    for packet in dropped_packets.into_iter() {
+      // TODO: if this fails, a bunch of packets are dropped
+      try!(self.single_send(packet));
+    }
+    self.single_send(p)
+  }
+
+  ///
+  /// - Get and increment sequence number
+  /// - Remember packet
+  /// - Add all headers
+  ///   - Sequence #
+  ///   - Current ack
+  ///   - Ack bitfield
+  /// - Send packet
+  fn single_send(&mut self, p: GafferPacket) -> io::Result<Option<usize>> {
+    let (destination, payload) = self.state.preprocess_packet(p);
+
+    self.udp_socket.send_to(payload.as_ref(), &destination)
+  }
+}
+
+#[cfg(test)]
+mod tests{
+
+  use super::*;
+  use packet::{GafferPayload, GafferPacket};
+
+  #[test]
+  fn recv_doesnt_block() {
+    let mut sock = GafferSocket::bind("0.0.0.0:45213").unwrap();
+
+    let payload = sock.recv();
+    assert!(payload.is_ok());
+    assert_eq!(payload.unwrap(), None);
+  }
+
+  #[test]
+  fn recv_can_recv() {
+    let mut send_sock = GafferSocket::bind("0.0.0.0:45214").unwrap();
+    let mut recv_sock = GafferSocket::bind("0.0.0.0:45215").unwrap();
+    let send_res = send_sock.send(GafferPacket::new("127.0.0.1:45215", GafferPayload::new()));
+    assert!(send_res.is_ok());
+    assert!(send_res.unwrap().is_some());
+
+
+    let packet = recv_sock.recv();
+    assert!(packet.is_ok());
+    let packet_payload = packet.unwrap();
+    assert!(packet_payload.is_some());
+    let addr = packet_payload.unwrap().addr;
+    assert_eq!(addr.to_string(), "127.0.0.1:45214");
+  }
 }
