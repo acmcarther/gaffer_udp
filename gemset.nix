7b4c5c424cdeab63833e2d55dca4739d017a25a4
Alex McArther <acmcarther@gmail.com>
2016-07-12 18:11:38 -0700

    Add nix files, update gemfile, and prep cargo file for cratepush (#5)

---
+++ gemset.nix
@@ -0,0 +1,66 @@
+{
+  multi_test = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "1sx356q81plr67hg16jfwz9hcqvnk03bd9n75pmdw8pfxjfy1yxd";
+      type = "gem";
+    };
+    version = "0.1.2";
+  };
+  multi_json = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "1rf3l4j3i11lybqzgq2jhszq7fh7gpmafjzd14ymp9cjfxqg596r";
+      type = "gem";
+    };
+    version = "1.11.2";
+  };
+  gherkin = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "055dirx8y1zn71jjgphha2aywbla0j69f2fgmxwncpy6csi0893i";
+      type = "gem";
+    };
+    version = "3.2.0";
+  };
+  diff-lcs = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "1vf9civd41bnqi6brr5d9jifdw73j9khc6fkhfl1f8r9cpkdvlx1";
+      type = "gem";
+    };
+    version = "1.2.5";
+  };
+  cucumber-wire = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "09ymvqb0sbw2if1nxg8rcj33sf0va88ancq5nmp8g01dfwzwma2f";
+      type = "gem";
+    };
+    version = "0.0.1";
+  };
+  cucumber-core = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "0f05hkrgn9h9yl2d7s92s9bd4q60y4p4smd4av838b4hfsz0jf38";
+      type = "gem";
+    };
+    version = "1.4.0";
+  };
+  cucumber = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "1jdimv1ajma9l14s28snqhmwbcc67d9ii7i8gxrjlfbi5pqd4zcg";
+      type = "gem";
+    };
+    version = "2.3.2";
+  };
+  builder = {
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "14fii7ab8qszrvsvhz6z2z3i4dw0h41a62fjr2h1j8m41vbrmyv2";
+      type = "gem";
+    };
+    version = "3.2.2";
+  };
+}
