e9097c40998f05185c033a279fc10219e7db7214
Alex McArther <acmcarther@gmail.com>
2017-02-03 17:59:46 -0800

    Missed one lol

--- src/socket/blocking.rs
+++ src/socket/blocking.rs
@@ -2,7 +2,8 @@ use addr::ToSingleSocketAddr;
 
 use packet::{
   CompleteGafferPacket,
-  GafferPacket
+  GafferPacket,
+  GAFFER_MTU,
 };
 
 use socket::GafferState;
@@ -14,7 +15,7 @@ use std::net::UdpSocket;
 pub struct GafferSocket {
   udp_socket: UdpSocket,
   state: GafferState,
-  recv_buffer: [u8; 8192]
+  recv_buffer: [u8; GAFFER_MTU],
 }
 
 impl GafferSocket {
@@ -24,7 +25,7 @@ impl GafferSocket {
       GafferSocket {
         udp_socket: sock,
         state: GafferState::new(),
-        recv_buffer: [0; 8192]
+        recv_buffer: [0; GAFFER_MTU]
       }
     })
   }
